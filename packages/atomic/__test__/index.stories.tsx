7d77b185dba4684c062317956b61fdde2a9f62bc
jinbo.wang <che3vinci@gmail.com>
2022-07-09 00:14:34 +0800

    feat: add storybook

--- packages/atomic/__test__/index.stories.tsx
+++ packages/atomic/__test__/index.stories.tsx
@@ -3,7 +3,10 @@ import { Text } from '../src';
 
 const Template = (args: any) => <Text {...args} />;
 export const Default = Template.bind({}) as any as { args: any };
-Default.args = {};
+Default.args = {
+  children:
+    'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat et ipsam id culpa facere ea repudiandae dicta qui? Asperiores dolores nihil provident magnam autem est ut quaerat eaque corporis odit.',
+};
 
 export default {
   component: Text,
