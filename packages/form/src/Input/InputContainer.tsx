3d43c9c987b9f6175133e576d4fc24e431641a45
jinbo.wang <che3vinci@gmail.com>
2022-07-24 21:43:25 +0800

    feat: input container css

--- packages/form/src/Input/InputContainer.tsx
+++ packages/form/src/Input/InputContainer.tsx
@@ -1,7 +1,7 @@
-import { BaseProps } from '@unstyled-ui/core';
+import { BaseProps, styled } from '@unstyled-ui/core';
 import { Item } from '@unstyled-ui/uikits';
 import { Row } from '@unstyled-ui/layout';
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, useMemo } from 'react';
 
 export type InputProps = {
   prefix?: JSX.Element;
@@ -11,21 +11,13 @@ export type InputProps = {
 } & BaseProps<InputHTMLAttributes<HTMLInputElement>>;
 
 export const InputContainer: React.FC<InputProps> = props => {
-  const {
-    prefix,
-    suffix,
-    allowClear,
-    children,
-    css = {},
-    ...restProps
-  } = props;
+  const { prefix, suffix, allowClear, children, ...restProps } = props;
   if (!React.isValidElement(props.children)) {
     throw new Error('TypeError:children must be reactElement');
   }
-  return (
-    <Row
-      as="u-input-container"
-      css={{
+  const _Row = useMemo(
+    () =>
+      styled(Row, {
         '& > input': {
           h: '100%',
           // w: 'max-content',
@@ -34,15 +26,20 @@ export const InputContainer: React.FC<InputProps> = props => {
           border: 'none',
         },
         '&:focus-within': {
-          // border: '1px solid $gray600',
+          border: '1px solid $gray600',
         },
-        ...css,
-      }}
-      {...restProps}
-    >
+      }),
+    []
+  );
+  return (
+    //@ts-ignore
+    <_Row as="u-input-container" {...restProps}>
       {prefix}
       {children}
       {suffix}
-    </Row>
+    </_Row>
+    // <Item direction="row" prefix={prefix} suffix={suffix} {...restProps}>
+    //   {children}
+    // </Item>
   );
 };
